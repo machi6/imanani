1cbad09e857c209f9daa903f780d105dfc6e285e
machi6 <yosukehonma01@gmail.com>
2020-09-22 20:44:26 +0900

    productを登録できるよう変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,19 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.valid?
+      @product.save
+      redirect_to issues_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name).merge(user_id: current_user.id)
+  end
 end
