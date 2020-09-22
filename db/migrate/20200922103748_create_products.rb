b953161ca779969411476052933be673b72e275b
machi6 <yosukehonma01@gmail.com>
2020-09-22 20:10:15 +0900

    productのマイグレーションファイルを記述

--- db/migrate/20200922103748_create_products.rb
+++ db/migrate/20200922103748_create_products.rb
@@ -1,6 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_031047) do
+ActiveRecord::Schema.define(version: 2020_09_22_103748) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_09_22_031047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
