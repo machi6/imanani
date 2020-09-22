ca8a565b099495a892ed6aea918c61a7eb990d2a
machi6 <yosukehonma01@gmail.com>
2020-09-22 19:37:32 +0900

    部品作成画面へのルーティングとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "issues#index"
+  resources :issues, only:[:index]
+  resources :products, only:[:new, :create] do
+    resources :issues, only:[:new, :create]
+  end
 end
