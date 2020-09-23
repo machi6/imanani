24f21b07dbfcdae167f971f349b182b5fdc5ddc9
machi6 <yosukehonma01@gmail.com>
2020-09-23 18:24:00 +0900

    issueのnewアクションとビューファイルを記述

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,4 +2,8 @@ class IssuesController < ApplicationController
   def index
     @products = Product.where(user_id: current_user.id)
   end
+  def new
+    @issue = Issue.new
+    @product = Product.find(params[:product_id])
+  end
 end
