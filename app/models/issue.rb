5b9dadf843d37bc2cb47588005862fd210a1cf4f
machi6 <yosukehonma01@gmail.com>
2020-09-23 19:31:07 +0900

    アイソレーションとバリデーションの記述

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,4 @@
 class Issue < ApplicationRecord
+  belongs_to :product
+  validates :summary, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  belongs_to :issue
+  with_options presence: true do
+    validates :title, presence: true
+    validates :start, presence: true
+    validates :time, presence: true
+  end
 end
