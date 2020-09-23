d5490244a8b62fbace8173e25c7698ef41bc2d69
machi6 <yosukehonma01@gmail.com>
2020-09-23 18:59:33 +0900

    issueのマイグレーションファイルを記述

--- db/migrate/20200923092449_create_issues.rb
+++ db/migrate/20200923092449_create_issues.rb
@@ -1,6 +1,9 @@
 class CreateIssues < ActiveRecord::Migration[6.0]
   def change
     create_table :issues do |t|
+      t.references :product, null: false, foreign_key: true
+      t.string :summary, null: false
+      t.text :description
 
       t.timestamps
     end
