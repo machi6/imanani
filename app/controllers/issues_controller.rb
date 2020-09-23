fd932834d6c22ba9fe0d33010b8178fd7412adc9
machi6 <yosukehonma01@gmail.com>
2020-09-23 19:03:59 +0900

    issueを登録できるよう変更

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,4 +6,18 @@ class IssuesController < ApplicationController
     @issue = Issue.new
     @product = Product.find(params[:product_id])
   end
+  def create
+    @issue = Issue.new(issue_params)
+    if @issue.valid?
+      @issue.save
+      redirect_to issues_path
+    else
+    redirect_to new_product_issue_path(params[:product_id])
+    end
+  end
+
+  private
+  def issue_params
+    params.require(:issue).permit(:summary).merge(product_id: params[:product_id])
+  end
 end
