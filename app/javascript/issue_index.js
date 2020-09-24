d8e0a1fbc0cfdca2552ade701c5830c0d2be7da9
machi6 <yosukehonma01@gmail.com>
2020-09-24 18:20:37 +0900

    div要素を設定

--- app/javascript/issue_index.js
+++ app/javascript/issue_index.js
@@ -8,14 +8,15 @@ window.addEventListener('load', function(){
     let start_time_M = Number(task_start_element[i].getAttribute("value").split(':')[1]);//15:30開始なら30
     let start_time_wday = task_wday_element[i].getAttribute("value");
     console.log(start_time_wday);
-    let start_pos = (start_time_H * 60) + (start_time_M) + (start_time_wday * 1440) - 360 ;
+    let start_pos = (start_time_H * 60) + (start_time_M) + (start_time_wday * 1440);
+    //let start_pos = (start_time_H * 60) + (start_time_M) + (start_time_wday * 1440) - 360 ;
 
     //let task_time_element = document.getElementsByClassName("task_time");
     let width = Number(task_time_element[i].getAttribute("value"));//1Hかかるなら3600(秒)
 
 
-    let task_wrapper = document.getElementById("task_wrapper_" + String(i));
-    task_wrapper.setAttribute("style", `width:${width/60}px; margin-left: ${start_pos}px;`);
+    let task_bar = document.getElementById("task_bar_" + String(i));
+    task_bar.setAttribute("style", `width:${width/60}px; margin-left: ${start_pos}px;`);
     let task_character = document.getElementById("task_character_" + String(i));
     task_character.setAttribute("style", `margin-left: ${start_pos}px;`)
   }
