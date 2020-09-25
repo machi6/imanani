0de21ba46710f2749b024a0dcf8694950a0d20b6
machi6 <yosukehonma01@gmail.com>
2020-09-25 14:45:51 +0900

    タスクのシフトをメソッド化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,52 +3,14 @@ class TasksController < ApplicationController
     @task = Task.new
   end
   def create
-    
     @task = Task.new(task_params)
     if @task.valid?
-      #以下の処理、テーブルの結合でうまくできないのか？
-      #担当部品のidを全て取得
-      all_products_id = Array.new
-      Product.all.each do |product| #userを登録後は、Product.all.where(current_user.id)とでもすればいいと思う
-        all_products_id << product.id
-      end
-      #担当部品が抱える課題のidを全て取得
-      all_issues_id = Array.new
-      all_products_id.each do |product_id|
-        all_issues_id.concat(Issue.where(product_id: product_id).ids)
-      end
-      #全ての課題のidを取得
-      all_tasks_id = Array.new
-      all_issues_id.each do |issue_id|
-        all_tasks_id.concat(Task.where(issue_id: issue_id).ids)
-      end
-      #自分の全てのタスクを取得
-      ordered_tasks = Task.where(id: all_tasks_id).order(start: "ASC")
-      
-      #新しいタスクが割り込みだったらそれ以降のタスクをシフトする
-      new_task_end = @task.start + @task.time.min * 60 + @task.time.hour * 3600
-      # new_task_man_hours = @task.time.min * 60 + @task.time.hour * 3600
-      difference = 0.0
-      changed = false
-      ordered_tasks.each do |task|
-        if changed == false
-          if @task.start < task.start && task.start < new_task_end
-            difference = new_task_end - task.start
-            task.update(start: task.start + difference)
-            changed = true
-            next
-          end
-        else
-          task.update(start: task.start + difference)
-        end
-      end
-
+      shift_task
       @task.save
       redirect_to issues_path
     else
       render :new
     end
-    
   end
 
   def edit
@@ -59,7 +21,9 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    if @task.update(task_params)
+    if @task.valid?
+      shift_task
+      @task.update(task_params)
       redirect_to issues_path
     else
       @product = Product.find(params[:product_id])
@@ -75,5 +39,42 @@ class TasksController < ApplicationController
   end
 
   def shift_task
+    #この処理、テーブルの結合でうまくできないのか？----------------------------------------------------------
+    #担当部品のidを全て取得
+    all_products_id = Array.new
+    Product.all.each do |product|
+      all_products_id << product.id
+    end
+    #担当部品が抱える課題のidを全て取得
+    all_issues_id = Array.new
+    all_products_id.each do |product_id|
+      all_issues_id.concat(Issue.where(product_id: product_id).ids)
+    end
+    #全ての課題のidを取得
+    all_tasks_id = Array.new
+    all_issues_id.each do |issue_id|
+      all_tasks_id.concat(Task.where(issue_id: issue_id).ids)
+    end
+    #自分の全てのタスクを取得
+    ordered_tasks = Task.where(id: all_tasks_id).order(start: "ASC")
+    #-------------------------------------------------------------------------------------------------
+    
+    #新しいタスクが割り込みだったらそれ以降のタスクをシフトする
+    new_task_end = @task.start + @task.time.min * 60 + @task.time.hour * 3600
+    # new_task_man_hours = @task.time.min * 60 + @task.time.hour * 3600
+    difference = 0.0
+    changed = false
+    ordered_tasks.each do |task|
+      if changed == false
+        if @task.start < task.start && task.start < new_task_end
+          difference = new_task_end - task.start
+          task.update(start: task.start + difference)
+          changed = true
+          next
+        end
+      else
+        task.update(start: task.start + difference)
+      end
+    end
   end
 end
