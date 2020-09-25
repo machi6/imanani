899fc5f1023a61cc871c111ab953e433504deece
machi6 <yosukehonma01@gmail.com>
2020-09-25 23:52:01 +0900

    taskをヘルパーメソッド化

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def tasks(tasks, issue, product)
+    html = ''
+    task_count = 0
+    tasks.where(issue_id: issue.id).each do |task|
+      html += render(partial: 'tasks/task', locals: {product: product, issue: issue, task: task, task_count: task_count})
+      task_count += 1
+    end
+    raw(html)
+  end
 end
