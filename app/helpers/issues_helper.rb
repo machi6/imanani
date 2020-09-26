a58ff28a7d52616a71e0f276b05406c4f147505f
machi6 <yosukehonma01@gmail.com>
2020-09-27 00:58:06 +0900

    縦線の表示をヘルパーメソッド化

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -8,4 +8,22 @@ module IssuesHelper
     end
     raw(html)
   end
+
+  def schedule_lines
+    html = ''
+    hour_ = 0
+    wday_ = ["日", "月", "火", "水", "木", "金", "土"]
+    wday_num = 0
+    168.times do |i|
+      if i % 24 == 0
+        hour_ = 0
+        html += render(partial: 'date_disp', locals: {wday_: wday_, wday_num: wday_num})
+        wday_num += 1
+      end
+      html += render(partial: 'time_disp', locals: {hour_: hour_, i: i})
+      html += render(partial: 'vertical_line', locals: {i: i})
+      hour_ += 1
+    end
+    raw(html)
+  end
 end
