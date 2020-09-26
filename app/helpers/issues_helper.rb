8c6645ceb5aa932ea86155cbd89a337aa24c51ee
machi6 <yosukehonma01@gmail.com>
2020-09-26 23:30:04 +0900

    issueをヘルパーメソッド化

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,11 @@
 module IssuesHelper
+  def issues(issues, product)
+    html = ''
+    issue_count = 0
+    issues.where(product_id: product.id).each do |issue|
+      html += render(partial: 'issue', locals: {product: product, issue: issue, issue_count: issue_count})
+      issue_count += 1
+    end
+    raw(html)
+  end
 end
