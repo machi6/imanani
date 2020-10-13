7c5762c8be8bf5263e3c04ad32c2ffb9f8f632b8
machi6 <yosukehonma01@gmail.com>
2020-10-13 10:02:25 +0900

    カラムの設定を追加してrails db:migrate

--- db/migrate/20201013004725_add_time_fixed_to_tasks.rb
+++ db/migrate/20201013004725_add_time_fixed_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddTimeFixedToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :time_fixed, :boolean
+    add_column :tasks, :time_fixed, :boolean, null: false, default: false
   end
 end
